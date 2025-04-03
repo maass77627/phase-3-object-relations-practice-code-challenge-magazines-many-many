2230256988a21a4cbe02f94ea92246214a47b6ed
kelly_maass <kmaass77627@gmail.com>
2025-04-03 17:47:27 -0500

    done

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,7 +2,10 @@ class Subscription < ActiveRecord::Base
   belongs_to :magazine
   belongs_to :reader
 
-def print_details
-   puts "#{self.reader.name} subscribed to #{self.magazine.title} for #{self.subscription.price}"
-end
+
+
+  def print_details
+    puts "#{self.reader.name} subscribed to #{self.magazine.title} for #{self.subscription.price}"
+ end
+
 end
